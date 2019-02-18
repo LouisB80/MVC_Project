c9632679638e88a19db789c4ede46f75dada184e
LouisB80 <Louis-bernard.75@hotmail.fr>
2019-02-18 15:18:18 +0100

    V3

--- Project1/Project1/Controllers/HomeController.cs
+++ Project1/Project1/Controllers/HomeController.cs
@@ -13,7 +13,6 @@ namespace Project1.Controllers
     public class HomeController : Controller
     {
         private AgendaContext db = new AgendaContext();
-
         // GET: Home
         public ActionResult HomePage()
         {
@@ -22,21 +21,17 @@ namespace Project1.Controllers
         }
 
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult AddAppointement()
+        public ActionResult AddBroker()
         {
             return View();
         }
-
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult AddBroker([Bind(Include = "LastName, FirstName, Mail, PhoneNumber")] Broker broker)
         {
             if (ModelState.IsValid)
             {
-                db.Brokers.Add(broker);
+                db.Brokers.Add(broker);                
                 db.SaveChanges();
                 return RedirectToAction("ListBrokers");
             }
@@ -44,6 +39,10 @@ namespace Project1.Controllers
         }
 
 
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult AddCustomer([Bind(Include = "LastName, FirstName, Mail, PhoneNumber, Budget, Subject")] Customer customer)
@@ -57,18 +56,53 @@ namespace Project1.Controllers
             return View();
         }
 
-
+        public ActionResult EditCustomer()
+        {
+            return View();
+        }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult EditCustomer([Bind(Include = "LastName, FirstName, Mail, PhoneNumber, Budget, Subject")] Customer customer)
         {
+            var CustomerToUpdate = db.Customers.Find(customer.CustomerID);
+            if (ModelState.IsValid)     
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                try
+                {                    
+                    db.SaveChanges();
+                    return RedirectToAction("ListCustomers");
+                }
+                catch (DataException)
+                {
+                    //Si echec:
+                    ModelState.AddModelError("", "Une erreur s'est produite, si le problème persiste appelez un technicien");
+                }
+            }
+            return View(CustomerToUpdate);
+        }
+
+
+        public ActionResult EditBroker()
+        {
             return View();
         }
-        
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult EditBroker([Bind(Include = "LastName, FirstName, Mail, PhoneNumber")]Broker brokers)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.Entry(brokers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListCustomer");
+            }
+            return View();
+        }
+
+
+        public ActionResult AddAppointement()
         {
             return View();
         }
@@ -116,5 +150,5 @@ namespace Project1.Controllers
             }
             return View(customer);
         }
-    }   
+    }
 }
